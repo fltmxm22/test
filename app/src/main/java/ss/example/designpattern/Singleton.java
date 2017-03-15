5909ec221475913892c3e5e7336815de28ab98a6
jhjeong <jhjeong@bnpinnovation.com>
2017-03-15 14:52:11 +0900

    test

--- app/src/main/java/ss/example/designpattern/Singleton.java
+++ app/src/main/java/ss/example/designpattern/Singleton.java
@@ -2,10 +2,6 @@ package ss.example.designpattern;
 
 import android.util.Log;
 
-/**
- * Created by SS on 2017-03-15.
- */
-
 public class Singleton {
     private static Singleton singleton = new Singleton();
 
@@ -16,9 +12,4 @@ public class Singleton {
     public static Singleton getInstance() {
         return singleton;
     }
-
-    public void print() {
-        Log.e(MainActivity.TAG, "It's print() method in Singleton instance.");
-    }
-
 }
