fd29d2086ef05f67683c75791e364a444f21ac46
jhjeong <jhjeong@bnpinnovation.com>
2017-03-15 19:00:39 +0900

    test

--- app/src/main/java/ss/example/designpattern/TemplateMethod/AbstractDisplay.java
+++ app/src/main/java/ss/example/designpattern/TemplateMethod/AbstractDisplay.java
@@ -4,16 +4,14 @@ package ss.example.designpattern.TemplateMethod;
  * Created by SS on 2017-03-15.
  */
 
-public abstract class AbstractDisplay {  //추상 클래스
+public abstract class AbstractDisplay {
 
-    // 하위 클래스에 구현을 맡기는 추상 메소드
     public abstract void open();
 
     public abstract void print();
 
     public abstract void close();
 
-    // 추상 클래스에서 구현되고 있는 메소드 display
     public final void display() {
         open();
         for (int i = 0; i < 5; i++)
